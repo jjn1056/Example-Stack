e2f8e54f77cd1e1d0309954857b90c92f2b1e1d8
John Napiorkowski <jjn1056@yahoo.com>
2012-06-29 08:16:12 -0400

    docs!

--- lib/Example/Stack.pm
+++ lib/Example/Stack.pm
@@ -4,6 +4,7 @@ use Moose;
 
 has stack => (
   traits  => ['Array'],
+  isa => 'ArrayRef[Int]',
   is => 'bare',
   default => sub { [] },
   handles => {
@@ -45,3 +46,65 @@ around pop => sub {
 sub min_naive { (shift->sort)[0] }
 
 __PACKAGE__->meta->make_immutable;
+
+=head1 NAME
+
+Example::Stack - Trying to write a min function that is fast on a Stack object
+
+=head1 SYNOPSIS
+
+    use Example::Stack;
+
+    my $stack = Example::Stack->new;
+
+    $stack->push($_) for (1,1,10);
+
+    say $stack->pop; # 10
+    $stack->push(100);
+    say $stack->min; # 1;
+
+=head1 DESCRIPTION
+
+Create a classic stack object which defines an additional method called C<min>
+that is a fast lookup of the minimum value in the stack.
+
+=head1 METHODS
+
+This class exposes the following public methods.
+
+=head2 push
+
+Pushes an Integer onto the stack.
+
+=head2 pop
+
+Pops a value of the stack
+
+=head2 min_naive
+
+Each time this method is called we do an ascending sort of ALL the items in
+the stack and return the first, which is the smallest value.
+
+=head2 min_fast
+
+Returns the smallest item in the stack by using a system where we track the
+smallest number history in a trace log.
+
+=head1 SEE ALSO
+
+The following modules or resources may be of interest.
+
+L<Moose>
+
+=head1 AUTHOR
+
+    John Napiorkowski C<< <jjnapiork@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012, John Napiorkowski C<< <jjnapiork@cpan.org> >>
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
